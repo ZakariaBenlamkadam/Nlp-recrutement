6bfbf41f570fd468c5bce160713139d2a1061e96
ZakariaBenlamkadam <benlamkadamzakaria@gmail.com>
2024-09-02 11:28:24 +0100

    added question generation based on job description

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,17 +1,15 @@
-import React from 'react';
-import './Footer.css';
+import React from 'react'
+import './Footer.css'
 
-function Footer() {
+export default function Footer() {
   return (
     <footer className="footer">
-      <div className="footer-text">&copy; 2024 Resume Matcher. All rights reserved.</div>
-      <nav className="footer-nav">
-        <a href="#" className="nav-link">Privacy</a>
-        <a href="#" className="nav-link">Terms</a>
-        <a href="#" className="nav-link">Support</a>
-      </nav>
-    </footer>
-  );
+        <div className="footer-text">&copy; 2024 Team 3D. All rights reserved.</div>
+        <nav className="footer-nav">
+          <a href="#" className="nav-link">Privacy</a>
+          <a href="#" className="nav-link">Terms</a>
+          <a href="#" className="nav-link">Support</a>
+        </nav>
+      </footer>
+  )
 }
-
-export default Footer;
