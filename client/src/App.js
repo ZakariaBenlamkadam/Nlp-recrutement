a6a08ca16fb79c3a9a61c5016c6142758568ed39
ZakariaBenlamkadam <benlamkadamzakaria@gmail.com>
2024-09-10 16:40:03 +0100

    add navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
-import Footer from './components/Footer';
-import Header from './components/Header';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout'; // Import the Layout component
 import LandingPage from './components/LandingPage';
 import Main from './components/Main';
 import QuestionGeneration from './components/QuestionGeneration';
 
 function App() {
-  
-
   return (
-    <div className="container">
-      <LandingPage/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<LandingPage />} />
+          <Route path="resume-match" element={<Main />} />
+          <Route path="quest-ai" element={<QuestionGeneration />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
