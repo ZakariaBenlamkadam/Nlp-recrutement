6a4ec4b8aed0fffcdf1ddbfb7777d5f8e042160c
ZakariaBenlamkadam <benlamkadamzakaria@gmail.com>
2024-09-10 15:33:18 +0100

    adding landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import LandingPage from './components/LandingPage';
 import Main from './components/Main';
 import QuestionGeneration from './components/QuestionGeneration';
 
@@ -9,9 +10,7 @@ function App() {
 
   return (
     <div className="container">
-      <Header/>
-      <QuestionGeneration/>
-      <Footer/>
+      <LandingPage/>
     </div>
   );
 }
